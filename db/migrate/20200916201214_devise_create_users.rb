b5b5390d6aad41cdff54b34158fc1a7c614acbbe
Tim <tjones4413@gmail.com>
2020-09-16 16:39:35 -0400

    Remove first_name and last_name fields and add name

--- db/migrate/20200916201214_devise_create_users.rb
+++ db/migrate/20200916201214_devise_create_users.rb
@@ -32,8 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :first_name
-      t.string :last_name
+      t.string :name
 
       t.timestamps null: false
     end
