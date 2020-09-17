f0ad100de219cadd23f3856c5c74e49d803622e6
Tim <tjones4413@gmail.com>
2020-09-17 14:58:24 -0400

    Create bugs migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_201214) do
+ActiveRecord::Schema.define(version: 2020_09_17_185119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bugs", force: :cascade do |t|
+    t.string "title"
+    t.integer "status"
+    t.integer "platform"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
