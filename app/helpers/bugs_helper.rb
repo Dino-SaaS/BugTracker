9d3ebc526cd4a8a434f1d428a5dce9814bf8409e
Tim <tjones4413@gmail.com>
2020-09-18 15:06:23 -0400

    Render real Bug data in table

--- app/helpers/bugs_helper.rb
+++ app/helpers/bugs_helper.rb
@@ -1,2 +1,20 @@
 module BugsHelper
+  def render_status(bug)
+    bug.status.gsub('_',' ').capitalize
+  end
+
+  def status_icon_class(bug)
+    case bug.status
+    when 'open'
+      'bg-primary'
+    when 'in_progress'
+      'bg-warning'
+    when 'closed'
+      'bg-success'
+    end
+  end
+
+  def render_date(date)
+    date.strftime('%m-%d-%Y at %I:%M%p')
+  end
 end
