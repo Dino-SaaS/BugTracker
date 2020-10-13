ada6f93a2be06f25758b00b6f379abfc61219366
Tim <tjones4413@gmail.com>
2020-10-13 17:44:07 -0400

    Remove pages_controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,4 @@ Rails.application.routes.draw do
   }
 
   resources :bugs
-
-  get 'pages/dashboard'
-  get 'pages/icons'
-  get 'pages/profile'
-  get 'pages/tables'
-  get 'pages/login'
-  get 'pages/register'
-  get 'pages/upgrade'
 end
