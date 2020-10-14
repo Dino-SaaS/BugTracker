219cd2cc1898480a5fd40db198d9e02d13e357a6
Tim <tjones4413@gmail.com>
2020-10-14 09:12:52 -0400

    Add Account model and associations

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -2,6 +2,8 @@ class Bug < ApplicationRecord
   enum platform: %i[web ios android]
   enum status:   %i[open in_progress closed]
 
+  belongs_to :account
+
   validates_presence_of %i[title platform status]
 
   has_rich_text :description
---
+++ db/migrate/20201014124623_add_account_to_bugs.rb
@@ -0,0 +1,5 @@
+class AddAccountToBugs < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :bugs, :account, null: false, foreign_key: true
+  end
+end
